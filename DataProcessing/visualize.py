c7987050717d5f42fa39973cc26024d74e330c5a
Cuongtd193245 <150518619+Cuongtd193245@users.noreply.github.com>
2023-12-30 17:16:59 +0700

    Visualize

---
+++ DataProcessing/visualize.py
@@ -0,0 +1,223 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json
+import tqdm
+from tqdm import tqdm
+from collections import OrderedDict
+
+raw_df = pd.read_csv('data/twitter_account_dataset.csv')
+raw_df = raw_df.drop(['Unnamed: 0'], axis=1)
+
+
+raw_df['account_type'] = raw_df['account_type'].astype('category')
+plt.title('Number of accounts in each type')
+plt.show()
+
+
+raw_df['bot'] = raw_df['account_type'].apply(lambda x: 1 if x == 'bot' else 0)
+raw_df['default_profile'] = raw_df['default_profile'].astype(int)
+raw_df['default_profile'] = raw_df['default_profile'].astype(int)
+raw_df['default_profile_image'] = raw_df['default_profile_image'].astype(int)
+raw_df['geo_enabled'] = raw_df['geo_enabled'].astype(int)
+raw_df['verified'] = raw_df['verified'].astype(int)
+
+# datetime conversion
+raw_df['created_at'] = pd.to_datetime(raw_df['created_at'])
+# hour created
+raw_df['hour_created'] = pd.to_datetime(raw_df['created_at']).dt.hour
+raw_df['year'] = pd.to_datetime(raw_df['created_at']).dt.year
+
+year_dict = raw_df['year'].value_counts().to_dict()
+
+default_profile_dict = raw_df['default_profile'].value_counts().to_dict()
+print(default_profile_dict)
+# visualize, use pie chart follow clockwise with a different palette and explode
+plt.pie(default_profile_dict.values(), labels=default_profile_dict.keys(), autopct='%1.1f%%', startangle=90,
+        colors=sns.color_palette("Paired"), explode=(0, 0.1))
+plt.title('Percentage of accounts with default profile')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+for i in year_dict:
+    plt.text(i, year_dict[i], year_dict[i], ha='center', va='bottom')
+plt.bar(year_dict.keys(), year_dict.values())
+plt.title('Number of accounts created in each year')
+plt.xlabel('Year')
+plt.ylabel('Number of accounts')
+plt.show()
+
+# usable df setup
+df = raw_df[['bot', 'screen_name', 'created_at', 'hour_created', 'verified', 'location', 'geo_enabled', 'lang', 'default_profile', 
+              'default_profile_image', 'favourites_count', 'followers_count', 'friends_count', 'statuses_count',
+             'average_tweets_per_day', 'account_age_days']]
+
+del raw_df
+
+
+
+# Interesting features to look at: 
+df['avg_daily_followers'] = np.round(df['followers_count'] / df['account_age_days'])
+df['avg_daily_friends'] = np.round(df['followers_count'] / df['account_age_days'])
+df['avg_daily_favorites'] = np.round(df['followers_count'] / df['account_age_days'])
+
+# Log transformations for highly skewed data
+df['friends_log'] = np.round(np.log(1 + df['friends_count']), 3)
+df['followers_log'] = np.round(np.log(1 + df['followers_count']), 3)
+df['favs_log'] = np.round(np.log(1 + df['favourites_count']), 3)
+df['avg_daily_tweets_log'] = np.round(np.log(1+ df['average_tweets_per_day']), 3)
+
+# Possible interaction features
+df['network'] = np.round(df['friends_log'] * df['followers_log'], 3)
+df['tweet_to_followers'] = np.round(np.log( 1+ df['statuses_count']) * np.log(1+ df['followers_count']), 3)
+
+# Log-transformed daily acquisition metrics for dist. plots
+df['follower_acq_rate'] = np.round(np.log(1 + (df['followers_count'] / df['account_age_days'])), 3)
+df['friends_acq_rate'] = np.round(np.log(1 + (df['friends_count'] / df['account_age_days'])), 3)
+df['favs_rate'] = np.round(np.log(1 + (df['friends_count'] / df['account_age_days'])), 3)
+
+
+# select oly numeric columns
+df_numeric = df.select_dtypes(include=[np.number])
+
+plt.figure(figsize=(8,6), dpi=100)
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+sns.heatmap(df_numeric.corr(), cmap=cmap, annot=False)
+plt.title('Correlation of potential features: all data', fontsize=14)
+plt.show()
+
+
+# Account age plots
+plt.figure(figsize=(7,5), dpi=100)
+sns.histplot(x='account_age_days', data=df, hue='bot', alpha=.25, 
+             kde=True, stat='density', common_bins=True, element='step', legend=True)
+
+plt.legend(title='Account Type', loc='upper right', labels=['Bot', 'Human'], frameon=False)
+plt.title('Distribution of Account Ages', fontsize=12)
+plt.xlabel('Account Age (# days)', fontsize=10)
+plt.ylabel('Density', fontsize=10)
+sns.despine();
+plt.show()
+
+
+
+# Followers count plots
+plt.figure(figsize=(7,5), dpi=100)
+sns.histplot(x='followers_log', data=df, hue='bot', alpha=.25, 
+             kde=True, stat='density', common_bins=True, element='step', legend=True)
+
+plt.legend(title='Account Type', loc='upper right', labels=['Bot', 'Human'], frameon=False)
+plt.title('Distribution of # Followers', fontsize=12)
+plt.xlabel('Followers (Log10 scale)', fontsize=10)
+plt.ylabel('Density', fontsize=10)
+plt.xlim(0, 18)
+plt.ylim(0, 0.11)
+sns.despine();
+plt.show()
+
+
+
+# Friends count plots (following)
+plt.figure(figsize=(7,5), dpi=100)
+sns.histplot(x='friends_log', data=df, hue='bot', alpha=.25, 
+             kde=True, stat='density', common_bins=True, element='step', legend=True)
+
+plt.legend(title='Account Type', loc='upper right', labels=['Bot', 'Human'], frameon=False)
+plt.title('Distribution of Friends (# Following)', fontsize=12)
+plt.xlabel('Friends Count (Log10 scale)', fontsize=10)
+plt.ylabel('Density', fontsize=10)
+plt.xlim(0, 16)
+plt.ylim(0, 0.65)
+sns.despine();
+plt.show()
+
+plt.figure(figsize=(12,5), dpi=100)
+plt.suptitle('Distribution of Followers & Friends', fontsize=14)
+
+plt.subplot(1, 2, 1)
+sns.histplot(x='followers_log', data=df, hue='bot', alpha=.25, 
+             kde=True, stat='density', common_bins=True, element='step', legend=False)
+plt.title('Distribution of # Followers', fontsize=12)
+plt.xlabel('Followers (Log10 scale)', fontsize=10)
+plt.ylabel('Density', fontsize=10)
+plt.xlim(0, 18)
+plt.ylim(0, 0.25)
+sns.despine()
+
+plt.subplot(1, 2, 2)
+sns.histplot(x='friends_log', data=df, hue='bot', alpha=.25, 
+             kde=True, stat='density', common_bins=True, element='step', legend=True)
+
+plt.legend(title='Account Type', loc='upper right', labels=['Bot', 'Human'], frameon=False)
+plt.title('Distribution of Friends (# Following)', fontsize=12)
+plt.xlabel('Friends Count (Log10 scale)', fontsize=10)
+plt.ylabel("")
+plt.yticks([])
+plt.xlim(0, 18)
+plt.ylim(0, 0.25)
+sns.despine()
+plt.show()
+
+
+# avg tweets per day
+plt.figure(figsize=(7,5), dpi=100)
+sns.histplot(x='avg_daily_tweets_log', data=df, hue='bot', alpha=.25, 
+             kde=True, stat='density', common_bins=True, element='step', legend=True)
+
+plt.legend(title='Account Type', loc='upper right', labels=['Bot', 'Human'], frameon=False)
+plt.title("Distribution of Tweets per Day", fontsize=12)
+plt.xlabel('Average Tweets per Day (Log10 scale)', fontsize=10)
+plt.ylabel('Density', fontsize=10)
+plt.xlim(0, 5)
+plt.ylim(0, 0.7)
+sns.despine();
+plt.show()
+
+
+# favorites count
+plt.figure(figsize=(7,5), dpi=100)
+sns.histplot(x='favs_log', data=df, hue='bot', alpha=.25, 
+             kde=True, stat='density', common_bins=True, element='step', legend=True)
+
+plt.legend(title='Account Type', loc='upper right', labels=['Bot', 'Human'], frameon=False)
+plt.title("Distribution of Favorites Count", fontsize=12)
+plt.xlabel('Favorites Count(Log10 scale)', fontsize=10)
+plt.ylabel('Density', fontsize=10)
+plt.xlim(0, 14)
+plt.ylim(0, 0.3)
+sns.despine();
+plt.show()
+
+
+# time account created
+plt.figure(figsize=(7,5), dpi=100)
+sns.histplot(x='hour_created', data=df, hue='bot', alpha=.25, 
+             kde=True, stat='density', common_bins=True, element='step', legend=True)
+
+plt.legend(title='Account Type', loc='best', labels=['Bot', 'Human'])
+plt.title("Time of Day Account was Created", fontsize=12)
+plt.xlabel('Time of Day (24h)', fontsize=10)
+plt.ylabel('Density', fontsize=10)
+plt.xlim(0, 24)
+plt.ylim(0, 0.05)
+sns.despine();
+#plt.savefig('imgs/hour_created.png');
+plt.show()
+
+
+# Network size
+# define as (number of followers) * (number of friends) (log10 scale)
+plt.figure(figsize=(7,5), dpi=100)
+sns.histplot(x='network', data=df, hue='bot', alpha=.25, 
+             kde=True, stat='density', common_bins=True, element='step', legend=True)
+
+plt.legend(title='Account Type', loc='upper right', labels=['Bot', 'Human'], frameon=False)
+plt.title('Distribution of Network Size', fontsize=12)
+plt.xlabel('Network Size [Friends * Followers] (Log10 scale)', fontsize=10)
+plt.ylabel('Density', fontsize=10)
+plt.xlim(0, 150)
+plt.ylim(0, 0.05)
+sns.despine();
+plt.show()
